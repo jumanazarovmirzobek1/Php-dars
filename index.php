dd70d1ae5a691f76d41f70000389fc337723423d
User <jumanazarovmirzobek201@gmail.com>
2026-04-04 18:02:16 +0500

    class va object yaratildi

--- index.php
+++ index.php
@@ -1,10 +1,41 @@
-<?php
-$massiv = [85,15,57,68,18,67,7,45,69,21,1,5,98,34];
-$m = 92;
-$yigindi=0;
-for($i=0;$i<count($massiv);$i++){
- if($massiv[$i]<$m){
-  $yigindi+=$massiv[$i]**2;
- }
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <?php
+    class Avtomobil{
+    public$name;
+    public$color;
+    public$year;
+    public$model;
+
+  function __construct($name,$color,$year,$model){
+    $this->name = $name;
+    $this->color = $color;
+    $this->year = $year;
+    $this->model = $model;
 }
-echo $yigindi;
+    function mirzobek(){
+        echo "yangi avtomobil nomi"."".$this->name."rang "." ".$this->color." "."ishlab chiqargan yili"." ".$this->year." ".$this->model."<br>";
+    }
+
+  
+
+  }
+  $bmw= new Avtomobil("bmw","qora",2020,"m5");
+  $bmw-> mirzobek();
+    
+   $bmw= new Avtomobil("merd","kok",2021,"g94");
+ $bmw-> mirzobek();
+
+   $bmw= new Avtomobil("chevrolet","qora",2024,"molibu");
+ $bmw-> mirzobek();
+
+    ?>
+</body>
+</html>
+
