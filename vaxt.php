23315e1fe4056057ef0a82a52ab10813508a18e1
User <jumanazarovmirzobek201@gmail.com>
2026-05-07 17:48:24 +0500

    date

---
+++ vaxt.php
@@ -0,0 +1,8 @@
+<?php
+echo "bugun " . date("Y/m/d") . "<br>";
+echo "bugun " . date("Y.m.d") . "<br>";
+echo "bugun". date("y/m/d") ."<br>";
+echo "bugun" . date("y/m/d")."<br>";
+echo "bugun" .date("i")."<br>";
+echo date("y/m/d");
+?>
